49a88a64bf19d821c5471bab647e260a1b7963d6
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-02-17 19:42:57 +0900

    update devDependencies
    Highlights: update @shinnn/eslint-config and follow its coding rules, for example tab indent

--- test-stdin.js
+++ test-stdin.js
@@ -6,18 +6,18 @@ const {strictEqual} = require('assert');
 require('loud-rejection/register');
 
 (async () => {
-  strictEqual(
-    await fileOrStdin(null, {encoding: 'base64'}),
-    Buffer.from('* text=auto\n').toString('base64'),
-    'should respect encoding option.'
-  );
+	strictEqual(
+		await fileOrStdin(null, {encoding: 'base64'}),
+		Buffer.from('* text=auto\n').toString('base64'),
+		'should respect encoding option.'
+	);
 
-  process.stdin.removeAllListeners('readable');
-  process.stdin.removeAllListeners('end');
+	process.stdin.removeAllListeners('readable');
+	process.stdin.removeAllListeners('end');
 
-  strictEqual(
-    await fileOrStdin(null, {encoding: 'UTF-8'}),
-    '* text=auto\n',
-    'should respect encoding option.'
-  );
+	strictEqual(
+		await fileOrStdin(null, {encoding: 'UTF-8'}),
+		'* text=auto\n',
+		'should respect encoding option.'
+	);
 })();
--- test.js
+++ test.js
@@ -4,52 +4,52 @@ const fileOrStdin = require('.');
 const {test} = require('tape');
 
 test('fileOrStdin', t => {
-  t.plan(6);
+	t.plan(6);
 
-  fileOrStdin(null).then(data => {
-    t.ok(
-      data.equals(Buffer.alloc(0)),
-      'should be resolve with an emtpty buffer when no data is passed to stdin.'
-    );
-  }).catch(t.fail);
+	fileOrStdin(null).then(data => {
+		t.ok(
+			data.equals(Buffer.alloc(0)),
+			'should be resolve with an emtpty buffer when no data is passed to stdin.'
+		);
+	}).catch(t.fail);
 
-  fileOrStdin(null, 'ucs2').then(data => {
-    t.strictEqual(
-      data,
-      '',
-      'should be resolve with an emtpty string when no data is passed to stdin and encoding is specified.'
-    );
-  }).catch(t.fail);
+	fileOrStdin(null, 'ucs2').then(data => {
+		t.equal(
+			data,
+			'',
+			'should be resolve with an emtpty string when no data is passed to stdin and encoding is specified.'
+		);
+	}).catch(t.fail);
 
-  fileOrStdin('.gitattributes', 'utf8').then(data => {
-    t.strictEqual(
-      data,
-      '* text=auto\n',
-      'should read a given file.'
-    );
-  }).catch(t.fail);
+	fileOrStdin('.gitattributes', 'utf8').then(data => {
+		t.equal(
+			data,
+			'* text=auto\n',
+			'should read a given file.'
+		);
+	}).catch(t.fail);
 
-  fileOrStdin('__this/file/does/not/exist__', {}).then(t.fail, err => {
-    t.strictEqual(
-      err.code,
-      'ENOENT',
-      'should fail when it cannot read a given file.'
-    );
-  }).catch(t.fail);
+	fileOrStdin('__this/file/does/not/exist__', {}).then(t.fail, ({code}) => {
+		t.equal(
+			code,
+			'ENOENT',
+			'should fail when it cannot read a given file.'
+		);
+	}).catch(t.fail);
 
-  fileOrStdin('file_path', 0).then(t.fail, err => {
-    t.strictEqual(
-      err.message,
-      'Expected an object or a string, but got 0 (number).',
-      'should fail when the second argument is neither an object nor a string.'
-    );
-  }).catch(t.fail);
+	fileOrStdin('file_path', 0).then(t.fail, ({message}) => {
+		t.equal(
+			message,
+			'Expected an object or a string, but got 0 (number).',
+			'should fail when the second argument is neither an object nor a string.'
+		);
+	}).catch(t.fail);
 
-  fileOrStdin('file_path', '').then(t.fail, err => {
-    t.strictEqual(
-      err.message,
-      'Expected a valid encoding (for example `utf8` and `base64`), but got \'\' (empty string).',
-      'should fail when the second argument is an empty string.'
-    );
-  }).catch(t.fail);
+	fileOrStdin('file_path', '').then(t.fail, ({message}) => {
+		t.equal(
+			message,
+			'Expected a valid encoding (for example `utf8` and `base64`), but got \'\' (empty string).',
+			'should fail when the second argument is an empty string.'
+		);
+	}).catch(t.fail);
 });
