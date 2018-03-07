363f59599b6d7f0b62e8df9c659d2bdbd758a629
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-03-07 10:40:58 +0900

    export async function
    [BREAKING] drop support for Node.js < 7

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ const {readFile} = require('graceful-fs');
 const getStdin = require('get-stdin');
 const inspectWithKind = require('inspect-with-kind');
 
-module.exports = function fileOrStdin(...args) {
+module.exports = async function fileOrStdin(...args) {
 	const argLen = args.length;
 
 	if (argLen !== 1 && argLen !== 2) {
-		return Promise.reject(new RangeError(`Expected 1 or 2 arguments (<string|Buffer|URL>[, <Object|string>]), but got ${
+		throw new RangeError(`Expected 1 or 2 arguments (<string|Buffer|URL>[, <Object|string>]), but got ${
 			argLen === 0 ? 'no' : argLen
-		} arguments.`));
+		} arguments.`);
 	}
 
 	const [filePath, options] = args;
 
 	if (options !== undefined && typeof options !== 'object' && typeof options !== 'string') {
-		return Promise.reject(new TypeError(`Expected an object or a string, but got ${
+		throw new TypeError(`Expected an object or a string, but got ${
 			inspectWithKind(options)
-		}.`));
+		}.`);
 	}
 
 	if (options === '') {
-		return Promise.reject(new Error('Expected a valid encoding (for example `utf8` and `base64`), but got \'\' (empty string).'));
+		throw new Error('Expected a valid encoding (for example `utf8` and `base64`), but got \'\' (empty string).');
 	}
 
 	if (filePath) {
@@ -44,11 +44,11 @@ module.exports = function fileOrStdin(...args) {
 		return getStdin();
 	}
 
-	const promise = getStdin.buffer();
+	const buf = await getStdin.buffer();
 
 	if (encoding) {
-		return promise.then(buf => buf.toString(encoding));
+		return buf.toString(encoding);
 	}
 
-	return promise;
+	return buf;
 };
